90c150499e35c633b88908b2378fa56e48d92307
iwpnd <legionaerr@googlemail.com>
2020-01-24 12:30:47 +0100

    changed config to use starlette config and .env. added loguru and intercepthandler

--- src/weightwhat/core/config.py
+++ src/weightwhat/core/config.py
@@ -1,6 +1,34 @@
-from starlette.datastructures import CommaSeparatedStrings
 import os
+import logging
+import sys
+from typing import List
+from databases import DatabaseURL
+
+from loguru import logger
+from starlette.datastructures import CommaSeparatedStrings
+from starlette.config import Config
+
+from weightwhat.core.logging import InterceptHandler
+
+config = Config(".env")
 
 ALLOWED_HOSTS = CommaSeparatedStrings(os.getenv("ALLOWED_HOSTS", ""))
-API_V1_STR = "/api/v1"
+API_PREFIX = "/api"
 PROJECT_NAME = "weight-what-API"
+VERSION = "0.0.1"
+
+DEBUG: bool = config("DEBUG", cast=bool, default=False)
+
+DATABASE_URL: DatabaseURL = config("DB_CONNECTION", cast=DatabaseURL)
+MAX_CONNECTIONS_COUNT: int = config("MAX_CONNECTIONS_COUNT", cast=int, default=10)
+MIN_CONNECTIONS_COUNT: int = config("MIN_CONNECTIONS_COUNT", cast=int, default=10)
+
+SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret)
+
+PROJECT_NAME: str = config("PROJECT_NAME", default="weight-what-API")
+LOGGING_LEVEL = logging.DEBUG if DEBUG else logging.INFO
+
+logging.basicConfig(
+    handlers=[InterceptHandler(level=LOGGING_LEVEL)], level=LOGGING_LEVEL
+)
+logger.configure(handlers=[{"sink": sys.stderr, "level": LOGGING_LEVEL}])
