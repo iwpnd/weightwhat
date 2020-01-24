362428273db1007867007767f62a06cc244c8f44
iwpnd <legionaerr@googlemail.com>
2020-01-24 17:11:23 +0100

    add database to main.py and add startup and shutdown event handlers

--- src/weightwhat/main.py
+++ src/weightwhat/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from weightwhat.api.api_v1.api import router as api_router
 from weightwhat.core.config import PROJECT_NAME, DEBUG, API_PREFIX, VERSION
+from weightwhat.db.db import metadata, database, engine
+from weightwhat.db.schemas import weights
+
+metadata.create_all(engine)
 
 
 app = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    await database.disconnect()
+
+
 app.include_router(api_router, prefix=API_PREFIX)
