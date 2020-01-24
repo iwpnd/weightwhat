4d166533dde1d06a6b349874b3091e46b609b280
iwpnd <legionaerr@googlemail.com>
2020-01-24 12:39:38 +0100

    directory restructure, added asyncpg and loguru to requirements, added api version to main.py

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,7 +1,7 @@
-from weightwhat.core.config import API_V1_STR
+from weightwhat.core.config import API_PREFIX
 
 
 def test_ping(test_app):
-    response = test_app.get(API_V1_STR + "/ping")
+    response = test_app.get(API_PREFIX + "/ping")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong"}
--- src/weightwhat/main.py
+++ src/weightwhat/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from weightwhat.api.api_v1.api import router as api_router
-from weightwhat.core.config import PROJECT_NAME, API_V1_STR
+from weightwhat.core.config import PROJECT_NAME, DEBUG, API_PREFIX, VERSION
 
 
-app = FastAPI(title=PROJECT_NAME)
-app.include_router(api_router, prefix=API_V1_STR)
+app = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
+app.include_router(api_router, prefix=API_PREFIX)
