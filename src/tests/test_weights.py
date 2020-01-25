7c42cd8e9ca4d756da00300c1ed35bf2e094a834
iwpnd <legionaerr@googlemail.com>
2020-01-25 15:22:17 +0100

    added test for GET /weights endpoint

--- src/tests/test_weights.py
+++ src/tests/test_weights.py
@@ -61,3 +61,48 @@ def test_get_weight_by_incorrect_id(test_app, monkeypatch):
 
     assert response.status_code == 404
     assert response.json()["detail"] == "weight id not found"
+
+
+def test_get_all_weights(test_app, monkeypatch):
+    test_data = [
+        {
+            "created_at": "2020-10-10T10:10:10",
+            "id": 1,
+            "updated_at": "2020-10-10T10:10:10",
+            "weight": 100.0,
+        },
+        {
+            "created_at": "2020-10-07T10:10:10",
+            "id": 2,
+            "updated_at": "2020-10-07T10:10:10",
+            "weight": 101.0,
+        },
+        {
+            "created_at": "2020-10-05T10:10:10",
+            "id": 3,
+            "updated_at": "2020-10-05T10:10:10",
+            "weight": 102.0,
+        },
+    ]
+
+    async def mock_get_all():
+        return test_data
+
+    monkeypatch.setattr(crud, "get_all", mock_get_all)
+
+    response = test_app.get(API_PREFIX + "/weights")
+
+    assert response.status_code == 200
+    assert response.json() == test_data
+
+
+def test_get_all_weights_fails(test_app, monkeypatch):
+    async def mock_get_all():
+        return None
+
+    monkeypatch.setattr(crud, "get_all", mock_get_all)
+
+    response = test_app.get(API_PREFIX + "/weights")
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == "no weights found"
