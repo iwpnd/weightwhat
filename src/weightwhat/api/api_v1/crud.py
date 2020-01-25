b83574f0fcc420c79269158479d5bc1b0986d350
iwpnd <legionaerr@googlemail.com>
2020-01-25 14:32:33 +0100

    added utility function get to crud.py

--- src/weightwhat/api/api_v1/crud.py
+++ src/weightwhat/api/api_v1/crud.py
@@ -12,3 +12,8 @@ async def post(payload: WeightSchema):
     query = weights.insert().values(weight=payload.weight)
     _log_query(query=str(query), query_params=query.parameters)
     return await database.execute(query)
+
+
+async def get(id: int):
+    query = weights.select().where(id == weights.c.id)
+    return await database.fetch_one(query=query)
