1fd72dcc37d5abfc923647cbdbd74d826728e804
iwpnd <legionaerr@googlemail.com>
2020-01-25 14:30:50 +0100

    build test cases for GET /weight/:id

--- src/tests/test_weights.py
+++ src/tests/test_weights.py
@@ -30,3 +30,34 @@ def test_create_weight_invalid_json(test_app):
         API_PREFIX + "/weight", data=json.dumps({"weight": "achtundsiebzigkommadrei"})
     )
     assert response.status_code == 422
+
+
+def test_get_weight_by_id(test_app, monkeypatch):
+    test_data = {
+        "created_at": "2020-10-10T10:10:10",
+        "id": 1,
+        "updated_at": "2020-10-10T10:10:10",
+        "weight": 100.0,
+    }
+
+    async def mock_get(id):
+        return test_data
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.get(API_PREFIX + "/weight/1")
+
+    assert response.status_code == 200
+    assert response.json() == test_data
+
+
+def test_get_weight_by_incorrect_id(test_app, monkeypatch):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.get(API_PREFIX + "/weight/99999")
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == "weight id not found"
