21288f217abef7ce9a737a31d5be37068032c2a2
iwpnd <legionaerr@googlemail.com>
2020-01-26 15:20:19 +0100

    reworked validators for WeightFromTo model. Now takes %Y%m%d or None as input. If None, then default is returned to date(1900,1,1) for fromdate and date.today() for todate. feels hacky, but works for now

--- src/weightwhat/models/models.py
+++ src/weightwhat/models/models.py
@@ -13,3 +13,22 @@ class WeightDB(WeightSchema):
     id: int = Field(..., title="Primary key of weight")
     created_at: datetime = Field(..., title="Created at %m/%d/%Y, %H:%M:%S")
     updated_at: datetime = None
+
+
+class WeightFromTo(BaseModel):
+    fromdate: date = None
+    todate: date = None
+
+    @validator("todate", pre=True, always=True)
+    def set_date_now(cls, v):
+        if isinstance(v, str):
+            return datetime.strptime(v, "%Y%m%d").date()
+        else:
+            return date.today()
+
+    @validator("fromdate", pre=True, always=True)
+    def set_date_past(cls, v):
+        if isinstance(v, str):
+            return datetime.strptime(v, "%Y%m%d").date()
+        else:
+            return date(1900, 1, 1)
