5c97c3269c282587038f83411a33feb617cf1d35
iwpnd <legionaerr@googlemail.com>
2020-01-26 11:00:56 +0100

    added new input class WeightFromTo with custom validator to allow date input following %Y%m%d schema

--- src/weightwhat/models/models.py
+++ src/weightwhat/models/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
-from datetime import datetime
+import pydantic
+from datetime import datetime, date
 
 
 class WeightSchema(BaseModel):
@@ -11,3 +12,22 @@ class WeightDB(WeightSchema):
     id: int = Field(..., title="Primary key of weight")
     created_at: datetime = Field(..., title="Created at %m/%d/%Y, %H:%M:%S")
     updated_at: datetime = None
+
+
+def parse_date(cls, value) -> date:
+    return datetime.strptime(value, "%Y%m%d").date()
+
+
+def date_validator(field: str) -> classmethod:
+    decorator = pydantic.validator(field, pre=True, allow_reuse=True)
+    validator = decorator(parse_date)
+    return validator
+
+
+class WeightFromTo(BaseModel):
+    fromdate: date = None
+    todate: date = None
+
+    # validators
+    _ensure_fromdate_is_normalized: classmethod = date_validator("fromdate")
+    _ensure_todate_is_normalized: classmethod = date_validator("todate")
