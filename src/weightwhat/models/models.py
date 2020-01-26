7dfff46c03a80df72b4de6a46e00a273b58ec955
iwpnd <legionaerr@googlemail.com>
2020-01-26 14:58:27 +0100

    reverted changes

--- src/weightwhat/models/models.py
+++ src/weightwhat/models/models.py
@@ -13,27 +13,3 @@ class WeightDB(WeightSchema):
     id: int = Field(..., title="Primary key of weight")
     created_at: datetime = Field(..., title="Created at %m/%d/%Y, %H:%M:%S")
     updated_at: datetime = None
-
-
-@dataclass
-class WeightFromTo(BaseModel):
-    fromdate: Optional[date] = date(1900, 1, 1)
-    todate: Optional[date] = date.today()
-
-    @validator("fromdate", pre=True, always=True, whole=True)
-    def parse_date_fromdate(cls, fromdate):
-        if not fromdate:
-            return date.today()
-        elif isinstance(fromdate, str):
-            return datetime.strptime(fromdate, "%Y%m%d").date()
-        else:
-            fromdate
-
-    @validator("todate", pre=True, always=True, whole=True)
-    def parse_date_todate(cls, todate):
-        if not todate:
-            return date.today()
-        elif isinstance(todate, str):
-            return datetime.strptime(todate, "%Y%m%d").date()
-        else:
-            todate
